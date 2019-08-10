167c9438fe387d0fc8dfd8bace4a1fff33496036
Alan Taranti <alan.taranti@gmail.com>
2019-08-10 11:59:53 -0400

    alterado o nome do arquivo para salvar

--- listener/stream_listener.py
+++ listener/stream_listener.py
@@ -15,7 +15,7 @@ class StreamListener(tweepy.StreamListener):
         # Save Control
         self.processed_tweets_counter = 0
         self.processed_tweets_to_save = 0
-        self.save_file = 'rawData'
+        self.save_file = 'raw_data'
         self.save_location = 'data/' + self.save_file + '.gzip'
 
         # Backup Control
