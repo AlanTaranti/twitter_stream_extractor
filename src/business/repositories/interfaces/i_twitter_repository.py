a1252889088899b82633e06878d6b9b1ef0fbbd8
Alan Taranti <contato@alan.taranti.me>
2022-08-12 23:40:39 -0400

    adicionado add_rules

--- src/business/repositories/interfaces/i_twitter_repository.py
+++ src/business/repositories/interfaces/i_twitter_repository.py
@@ -5,11 +5,14 @@ from enterprise.models.filter_rule import FilterRule
 
 
 class ITwitterRepository(metaclass=abc.ABCMeta):
-
     @abc.abstractmethod
     def get_stream_rules(self) -> List[FilterRule]:
         raise NotImplementedError
 
     @abc.abstractmethod
     def delete_stream_rules_by_ids(self, ids: List[int]) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def add_rules(self, rules: List[FilterRule]) -> List[FilterRule]:
         raise NotImplementedError
