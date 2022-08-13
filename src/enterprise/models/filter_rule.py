8b1e1ecb7cbde9598fb2cf354e9c5105e5d9ab1e
Alan Taranti <contato@alan.taranti.me>
2022-08-12 23:23:21 -0400

    tratado valores nulos e adicionado representacao em string

--- src/enterprise/models/filter_rule.py
+++ src/enterprise/models/filter_rule.py
@@ -1,5 +1,11 @@
 class FilterRule:
     def __init__(self, data):
-        self.id: int = data["id"]
-        self.value: str = data["value"]
-        self.tag = data["tag"]
+        self.id: int = data.get("id", None)
+        self.value: str = data.get("value", None)
+        self.tag: str = data.get("tag", None)
+
+    def __str__(self):
+        return f"{self.id} - {self.value} - {self.tag}"
+
+    def __repr__(self):
+        return self.__str__()
