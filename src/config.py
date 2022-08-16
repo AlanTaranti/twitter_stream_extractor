f075f1a76311c16fc192f28b79251305f853b974
Alan Taranti <contato@alan.taranti.me>
2022-08-16 18:06:16 -0400

    adicionado config de variaveis de ambiente

--- src/config.py
+++ src/config.py
@@ -3,8 +3,9 @@ from os.path import join, dirname
 
 from dotenv import load_dotenv
 
-# Environment Variables
-dotenv_path = join(dirname(__file__), '..', ".env")
+# Load Environment Variables
+dotenv_path = join(dirname(__file__), "..", ".env")
 load_dotenv(dotenv_path)
 
+# Configs
 BEARER_TOKEN = environ.get("BEARER_TOKEN")
