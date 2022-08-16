3cfae3641ee1f7b657576bbab053ba4b65a9f080
Alan Taranti <contato@alan.taranti.me>
2022-08-16 18:08:51 -0400

    implmentado metodo stream

--- src/infrastructure/repositories/implementation/twitter_repository.py
+++ src/infrastructure/repositories/implementation/twitter_repository.py
@@ -1,9 +1,12 @@
-from typing import List
+import json
+from typing import List, Callable
 
 import requests
+from requests import Response
 
 from business.repositories.interfaces.i_twitter_repository import ITwitterRepository
 from enterprise.models.filter_rule import FilterRule
+from enterprise.models.tweet import Tweet
 from infrastructure.exception.http_exception import HttpException
 
 from config import BEARER_TOKEN
@@ -23,13 +26,23 @@ class TwitterRepository(ITwitterRepository):
     def __convert_raw_data_to_filter_rule(data) -> List[FilterRule]:
         return [FilterRule(rule) for rule in data]
 
+    @staticmethod
+    def __convert_raw_data_to_tweet(raw_data) -> Tweet:
+        return Tweet(raw_data["data"])
+
+    @staticmethod
+    def __handle_error(response: Response, expected_status_code: int):
+        if response.status_code != expected_status_code:
+            raise HttpException(response.status_code, response.text)
+
     def get_stream_rules(self) -> List[FilterRule]:
         url = TWITTER_STREAM_URL + "/rules"
 
         # Get Data
         response = requests.get(url, auth=TwitterRepository.__requests_bearer_oauth)
-        if response.status_code != 200:
-            raise HttpException(response.status_code, response.text)
+
+        # Handle error
+        TwitterRepository.__handle_error(response, 200)
 
         # Parse Data
         response_content = response.json()
@@ -39,7 +52,7 @@ class TwitterRepository(ITwitterRepository):
             response_content.get("data", [])
         )
 
-    def delete_stream_rules_by_ids(self, ids: List[int]):
+    def delete_stream_rules_by_ids(self, ids: List[int]) -> None:
         url = TWITTER_STREAM_URL + "/rules"
 
         # Create Payload
@@ -53,8 +66,7 @@ class TwitterRepository(ITwitterRepository):
         )
 
         # Handle error
-        if response.status_code != 200:
-            raise HttpException(response.status_code, response.text)
+        TwitterRepository.__handle_error(response, 200)
 
     def add_rules(self, rules: List[FilterRule]) -> List[FilterRule]:
         url = TWITTER_STREAM_URL + "/rules"
@@ -70,8 +82,7 @@ class TwitterRepository(ITwitterRepository):
         )
 
         # Handle error
-        if response.status_code != 201:
-            raise HttpException(response.status_code, response.text)
+        TwitterRepository.__handle_error(response, 201)
 
         # Parse Data
         response_content = response.json()
@@ -80,3 +91,25 @@ class TwitterRepository(ITwitterRepository):
         return TwitterRepository.__convert_raw_data_to_filter_rule(
             response_content.get("data", [])
         )
+
+    def stream(self, callback_function: Callable[[Tweet], None]) -> None:
+        url = TWITTER_STREAM_URL + "?tweet.fields=context_annotations,lang"
+        print(url)
+        response = requests.get(
+            url,
+            auth=TwitterRepository.__requests_bearer_oauth,
+            stream=True,
+        )
+
+        # Handle error
+        TwitterRepository.__handle_error(response, 200)
+
+        # Stream
+        for response_line in response.iter_lines():
+            if response_line:
+                # Parse Data
+                json_response = json.loads(response_line)
+                tweet = TwitterRepository.__convert_raw_data_to_tweet(json_response)
+
+                # Callable
+                callback_function(tweet)
